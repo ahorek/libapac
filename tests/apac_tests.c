7fe6b448017864bc19c1a95451f002316d2034f6
Pavel Rosický <pavel.rosicky@easy.cz>
2025-10-28 12:25:07 +0100

    use cmp

--- tests/apac_tests.c
+++ tests/apac_tests.c
@@ -14,23 +14,20 @@ int test_apn_add_n() {
 
     apn_seg_t expected[3] = {5, 7, 9};
 
-    for (size_t i = 0; i < 3; i++) {
-        if (res[i] != expected[i]) {
-            fprintf(stderr, "Test failed at index %zu: got %u, expected %u\n",
-                    i, (unsigned int)res[i], (unsigned int)expected[i]);
-            return 1;
-        }
+    if (apn_cmp(res, expected, 3) == 0) {
+        printf("apn_add_n test passed!\n");
+        return 0;
+    } else {
+        printf("apn_add_n test failed!\n");
+        return 1;
     }
-
-    printf("apn_add_n test passed!\n");
-    return 0;
 }
 
 
 int main() {
     apacGetCPUSpec();
     if (test_apn_add_n()) {
-	return 1;
+	    return 1;
     }
 
     printf("All tests passed!\n");
