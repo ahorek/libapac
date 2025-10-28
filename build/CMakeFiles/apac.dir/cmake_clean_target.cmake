8ef44cf6d6b2a4c0d86cb1732d7c10b6f23f8453
Pavel Rosický <pavel.rosicky@easy.cz>
2025-10-28 11:00:40 +0100

    x

--- build/CMakeFiles/apac.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libapac.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahorek/liba/libapac
-# Build directory: /home/ahorek/liba/libapac/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ApacTests "/home/ahorek/liba/libapac/build/tests")
-set_tests_properties(ApacTests PROPERTIES  _BACKTRACE_TRIPLES "/home/ahorek/liba/libapac/CMakeLists.txt;206;add_test;/home/ahorek/liba/libapac/CMakeLists.txt;0;")
