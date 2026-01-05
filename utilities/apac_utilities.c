ca31a8c94440ee9eb1023b5b165f9915fa23c90e
Pavel Rosický <pavel.rosicky@easy.cz>
2026-01-05 19:34:32 +0100

    x

--- utilities/apac_utilities.c
+++ utilities/apac_utilities.c
@@ -28,10 +28,18 @@ uint64_t cpu_timer(void)
 #elif (defined(__GNUC__) || defined(__clang__)) && \
       (defined(__aarch64__) || defined(__arm64__))
 
+    __asm__ volatile("isb" ::: "memory");
+    uint64_t cnt, frq;
+    __asm__ volatile("mrs %0, cntvct_el0" : "=r"(cnt));
+    __asm__ volatile("mrs %0, cntfrq_el0" : "=r"(frq));
+    return (cnt * 1000000000ULL) / frq;
+
+    /*
     __isb();
     uint64_t cnt = __arm_rsr64("cntvct_el0");
     uint64_t frq = __arm_rsr64("cntfrq_el0");
     return (cnt * 1000000000ULL) / frq;
+    */
 
 #else
     #error "cpu_timer(): unsupported architecture or compiler"
