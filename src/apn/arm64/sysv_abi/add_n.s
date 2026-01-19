d56bd726395c0776c898394bab86787fbaccafcc
Pavel Rosický <pavel.rosicky@easy.cz>
2026-01-19 01:44:52 +0100

    x

--- src/apn/arm64/sysv_abi/add_n.s
+++ src/apn/arm64/sysv_abi/add_n.s
@@ -1,109 +1,51 @@
+//   O---------------------------------------------------------------------------O
+//   |                                                                           |
+//   |                   BALANCED ADDITION FUNCS (N LIMBS)                       |
+//   |                                                                           |
+//   O---------------------------------------------------------------------------O
 
-#   O---------------------------------------------------------------------------O
-#   |                                                                           |
-#   |                   BALANCED ADDITION FUNCS (N LIMBS)                       |
-#   |                                                                           |
-#   O---------------------------------------------------------------------------O
+    //   Function Arguments
+    //
+    //   x0 -> result   (apn_seg_t*)
+    //   x1 -> op1      (const apn_seg_t*)
+    //   x2 -> op2      (const apn_seg_t*)
+    //   x3 -> size     (apn_size_t)
 
-    #   Function Arguments
-    #
-    #   rdi -> result   (apn_seg_t*)
-    #   rsi -> op1      (const apn_seg_t*)
-    #   rdx -> op2      (const apn_seg_t*)
-    #   rcx -> size     (apn_size_t)
-
-.intel_syntax noprefix
 .text
-.globl add_n_x64, add_n_zen4
-.type  add_n_x64, @function
-.type  add_n_zen4, @function
+.globl add_n_arm64
+.type  add_n_arm64, @function
 
-add_n_zen4:
-.cfi_startproc
+add_n_arm64:
+    // Set up frame (optional)
+    // Save registers if needed
+    
+    // Clear carry flag
+    mov     x4, #0          // Use x4 as temporary for carry
+    
+    // Check if size is zero
+    cbz     x3, .Larm64_end_of_func
 
-    xor     rax, rax
-    mov     r11, rcx
-    shr     rcx, 2
-    and     r11, 3
-    jz      .Lzen4_before_main_loop
-
-.Lzen4_rmdr_loop:
-
-    mov     rax, [rsi]
-    adc     rax, [rdx]
-    mov     [rdi], rax
-    lea     rsi, [rsi + 8]
-    lea     rdx, [rdx + 8]
-    lea     rdi, [rdi + 8]
-    dec     r11
-    jnz     .Lzen4_rmdr_loop
-
-.Lzen4_before_main_loop:
-
-    setc    al
-    test    rcx, rcx
-    bt      ax, 0
-    jz      .Lzen4_end_of_func
-
-.p2align 6
-.Lzen4_unroll4_loop:
-
-.set i, 0
-.rept 4
-
-    mov     rax, QWORD PTR [rsi + i * 8]
-    adc     rax, QWORD PTR [rdx + i * 8]
-    mov     QWORD PTR [rdi + i * 8], rax
-
-.set i, i + 1
-.endr
-
-    lea     rsi, [rsi + 32]
-    lea     rdx, [rdx + 32]
-    lea     rdi, [rdi + 32]
-    dec     rcx
-    jnz     .Lzen4_unroll4_loop
+    // Main loop
+.p2align 4
+.Larm64_main_loop:
+    ldr     x5, [x1], #8    // Load op1 limb and post-increment
+    ldr     x6, [x2], #8    // Load op2 limb and post-increment
+    
+    // Add with carry: x5 = x5 + x6 + carry
+    adcs    x5, x5, x6
+    
+    str     x5, [x0], #8    // Store result and post-increment
+    
+    // Decrement counter and check for zero
+    subs    x3, x3, #1
+    bne     .Larm64_main_loop
 
 .p2align 4
-.Lzen4_end_of_func:
-
-    setc    al
-    movzx   rax, al
+.Larm64_end_of_func:
+    // Get final carry
+    cset    x0, cs          // Set x0 = 1 if carry set, else 0
     ret
 
-.cfi_endproc
-.size add_n_zen4, .-add_n_zen4
-
-add_n_x64:
-.cfi_startproc
-
-    xor     rax, rax
-    test    rcx, rcx
-    jz      .Lx64_end_of_func
-
-.p2align 4
-.Lx64_main_loop:
-
-    mov     rax, QWORD PTR [rsi]
-    adc     rax, QWORD PTR [rdx]
-    mov     QWORD PTR [rdi], rax
-
-    lea     rsi, [rsi + 8]
-    lea     rdx, [rdx + 8]
-    lea     rdi, [rdi + 8]
-
-    dec     rcx
-    jnz     .Lx64_main_loop
-
-.p2align 4
-.Lx64_end_of_func:
-
-    setc    al
-    movzx   rax, al
-    ret
-
-.cfi_endproc
-.size add_n_x64, .-add_n_x64
+.size add_n_arm64, .-add_n_arm64
 
 .section .note.GNU-stack,"",@progbits
-
--- src/apn/x64/sysv_abi/sub_n.s
+++ src/apn/x64/sysv_abi/sub_n.s
@@ -1,108 +1,48 @@
-#   O---------------------------------------------------------------------------O
-#   |                                                                           |
-#   |                   BALANCED SUBTRACTION FUNCS (N LIMBS)                    |
-#   |                                                                           |
-#   O---------------------------------------------------------------------------O
+//   O---------------------------------------------------------------------------O
+//   |                                                                           |
+//   |                   BALANCED SUBTRACTION FUNCS (N LIMBS)                    |
+//   |                                                                           |
+//   O---------------------------------------------------------------------------O
     
-    #   Function Arguments
-    #
-    #   rdi -> result   (apn_seg_t*)
-    #   rsi -> op1      (const apn_seg_t*)
-    #   rdx -> op2      (const apn_seg_t*)
-    #   rcx -> size     (apn_size_t)
+    //   Function Arguments
+    //
+    //   x0 -> result   (apn_seg_t*)
+    //   x1 -> op1      (const apn_seg_t*)
+    //   x2 -> op2      (const apn_seg_t*)
+    //   x3 -> size     (apn_size_t)
 
-.intel_syntax noprefix
 .text
-.globl sub_n_x64, sub_n_zen4
-.type  sub_n_x64, @function
-.type  sub_n_zen4, @function
+.globl sub_n_arm64
+.type  sub_n_arm64, @function
 
-sub_n_zen4:
-.cfi_startproc
-
-    xor     rax, rax
-    mov     r11, rcx            # r11 = size
-    shr     rcx, 2              # rcx = size / 4
-    and     r11, 3              # r11 = size % 4
-    jz      .Lzen4_before_unroll4
-
-.Lzen4_rmdr_loop:
-
-    mov     rax, [rsi]
-    sbb     rax, [rdx]
-    mov     [rdi], rax
-    lea     rsi, [rsi + 8]
-    lea     rdx, [rdx + 8]
-    lea     rdi, [rdi + 8]
-    dec     r11
-    jnz     .Lzen4_rmdr_loop
-
-.Lzen4_before_unroll4:
+sub_n_arm64:
+    // Clear borrow flag (carry flag in subtraction context)
+    mov     x4, #0          // Use x4 as temporary
     
-    setc    al
-    test    rcx, rcx
-    bt      ax, 0
-    jz      .Lzen4_end_of_func
+    // Check if size is zero
+    cbz     x3, .Larm64_end_of_func
+
+    // Main loop
+.p2align 4
+.Larm64_loop:
+    ldr     x5, [x1], #8    // Load op1 limb and post-increment
+    ldr     x6, [x2], #8    // Load op2 limb and post-increment
+    
+    // Subtract with borrow: x5 = x5 - x6 - borrow
+    sbcs    x5, x5, x6      // Sets carry flag = NOT borrow
+    
+    str     x5, [x0], #8    // Store result and post-increment
+    
+    // Decrement counter and check for zero
+    subs    x3, x3, #1
+    bne     .Larm64_loop
 
 .p2align 4
-.Lzen4_unroll4_loop:
-
-.set i, 0
-.rept 4
-    
-    mov     rax, QWORD PTR [rsi + i * 8]
-    sbb     rax, QWORD PTR [rdx + i * 8]
-    mov     QWORD PTR [rdi + i * 8], rax
-
-.set i, i + 1
-.endr
-
-    lea     rsi, [rsi + 32]
-    lea     rdx, [rdx + 32]
-    lea     rdi, [rdi + 32]
-    dec     rcx
-    jnz     .Lzen4_unroll4_loop
-
-.p2align 4
-.Lzen4_end_of_func:
-
-    setc    al
-    movzx   rax, al
+.Larm64_end_of_func:
+    // Get final borrow (carry clear = borrow occurred)
+    cset    x0, cc          // Set x0 = 1 if carry clear (borrow), else 0
     ret
 
-.cfi_endproc
-.size sub_n_zen4, .-sub_n_zen4
-
-sub_n_x64:
-.cfi_startproc
-    
-    xor     rax, rax
-    test    rcx, rcx
-    jz      .Lx64_end_of_func
-
-.p2align 4
-.Lx64_loop:
-
-    mov     rax, QWORD PTR [rsi]
-    sbb     rax, QWORD PTR [rdx]
-    mov     QWORD PTR [rdi], rax
-    
-    lea     rsi, [rsi + 8]
-    lea     rdx, [rdx + 8]
-    lea     rdi, [rdi + 8]
-    
-    dec     rcx
-    jnz     .Lx64_loop
-
-.p2align 4
-.Lx64_end_of_func:
-
-    setc    al
-    movzx   rax, al
-    ret
-
-.cfi_endproc
-.size sub_n_x64, .-sub_n_x64
+.size sub_n_arm64, .-sub_n_arm64
 
 .section .note.GNU-stack,"",@progbits
-
