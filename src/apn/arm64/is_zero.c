9831446f13470b9254b04ca12d9de1856178a592
Pavel Rosický <pavel.rosicky@easy.cz>
2026-01-20 16:27:13 +0100

    asimd

--- src/apn/arm64/is_zero.c
+++ src/apn/arm64/is_zero.c
@@ -5,14 +5,39 @@ int is_zero_asimd_4unroll(
     apn_size_t size
 )
 {
+    apn_size_t blocks = size - (size % 8);
     apn_size_t counter = 0;
+    
+    uint64x2_t zero = vdupq_n_u64(0);
 
-    while (counter < size)
+    while (counter < blocks)
+    {
+        // Load 8 values
+        uint64x2_t v0 = vld1q_u64(&op1[counter]);
+        uint64x2_t v1 = vld1q_u64(&op1[counter + 2]);
+        uint64x2_t v2 = vld1q_u64(&op1[counter + 4]);
+        uint64x2_t v3 = vld1q_u64(&op1[counter + 6]);
+
+        // OR all vectors together
+        v0 = vorrq_u64(v0, v1);
+        v2 = vorrq_u64(v2, v3);
+        v0 = vorrq_u64(v0, v2);
+
+        // Check if any non-zero: use horizontal OR reduction
+        uint64x1_t reduced = vorr_u64(vget_low_u64(v0), vget_high_u64(v0));
+        uint64_t result = vget_lane_u64(reduced, 0);
+        
+        if (result != 0)
+            return 1;
+
+        counter += 8;
+    }
+
+    // Handle remaining elements
+    for (; counter < size; counter++)
     {
         if (op1[counter] != 0ULL)
             return 1;
-
-        counter++;
     }
 
     return 0;
--- src/apn/arm64/set.c
+++ src/apn/arm64/set.c
@@ -1,17 +1,31 @@
 #include "../../../include/apac.h"
 
 void set_asimd_4unroll(
-	apn_seg_t* result,
-	apn_size_t size,
-	apn_seg_t val
+    apn_seg_t* result,
+    apn_size_t size,
+    apn_seg_t val
 )
-{	apn_size_t counter = 0;
+{
+    uint64x2_t my_val = vdupq_n_u64(val);
 
-	while (counter < size)
-	{
-		result[counter] = val;
-		counter++;
-	}
+    apn_size_t blocks = size - (size % 8);
+    apn_size_t counter = 0;
 
-	return;
+    while (counter < blocks)
+    {
+        // Store 8 values (4x uint64x2_t vectors)
+        vst1q_u64(&result[counter], my_val);
+        vst1q_u64(&result[counter + 2], my_val);
+        vst1q_u64(&result[counter + 4], my_val);
+        vst1q_u64(&result[counter + 6], my_val);
+
+        counter += 8;
+    }
+
+    // Handle remaining elements
+    while (counter < size)
+    {
+        result[counter] = val;
+        counter++;
+    }
 }
